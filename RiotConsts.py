d3b9e16c737b472fe10bf7b99e8587e1b7120be2
empzt <pirompzt@gmail.com>
2015-06-19 16:34:55 +0200

    Update RiotConsts.py

--- RiotConsts.py
+++ RiotConsts.py
@@ -1,6 +1,6 @@
 
 URL = {
-    'base': 'https://{proxy}.api.pvp.net/api/lol/{region}/{url}'
+    'base': 'https://{proxy}.api.pvp.net/api/lol/{region}/{url}',
     'summoner_by_name': 'v{version}/summoner/by-name/{names}'
 }
 
